082cf7d90581599b09e3b14462e15a598d735427
erosespinola <eros.espinola.gonzalez@gmail.com>
2014-03-30 15:53:25 -0600

    no talkbox

--- classifier.py
+++ classifier.py
@@ -1,16 +1,12 @@
 import scipy.io.wavfile
 from matplotlib.pyplot import specgram
 import numpy as np
-import scikits.talkbox
+#import scikits.talkbox
 
 def h(w, x):
 	return 1/(1 + np.exp(np.dot(w,x)))	
 
 # MAIN
-w = np.array([1,2,3])
-x = np.array([2,2,2])
-
-print h(w,x)
 
 W = np.zeros(())
 
