5ec6d6361199209b355e72b1a9c80a8d7ee4042d
Mauricio D Zaragoza Ibarra <davidz@cs.unm.edu>
2014-03-30 15:42:22 -0600

    printing of fft features

--- classifier.py
+++ classifier.py
@@ -1,2 +1,18 @@
+import scipy.io.wavfile
+from matplotlib.pyplot import specgram
 import numpy as np
 import scikits.talkbox
+
+
+W = np.zeros(())
+
+data_folder = 'opihi.cs.uvic.ca/sound/genres/'
+genres = ['classical', 'jazz', 'country', 'pop', 'rock', 'metal']
+
+sample_rate, X = scipy.io.wavfile.read(data_folder + "classical/classical.00000.wav")
+print sample_rate, X.shape
+fft_features = abs(scipy.fft(X)[:1000])
+print fft_features
+
+# specgram(X, Fs=sample_rate, xextent=(0, 30))
+
