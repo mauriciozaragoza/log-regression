7a0a53d1ec221ba9c6faa76380334dbe7e333e6c
erosespinola <eros.espinola.gonzalez@gmail.com>
2014-04-05 15:23:53 -0600

    mfcc features matrix

--- scikits.talkbox-0.2.5/docs/src/talkbox_version.py
+++
@@ -1,7 +0,0 @@
-# THIS FILE IS GENERATED FROM THE SETUP.PY. DO NOT EDIT.
-
-short_version='0.2.5'
-version=short_version
-dev=False
-if dev:
-    version += '.dev'
--- scikits/talkbox/tools/preprocess.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-__all__ = ["demean"]
-
-def demean(x, axis=-1):
-    return x - np.mean(x,axis)
